3bc8c3637874c32a05e49e1cf71fe28d853f53a4
Raja Faras <rajafaras08@gmail.com>
2026-07-13 13:02:45 +0700

    Create WhatsApp Gateway

---
+++ config/whatsapp.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'token' => env('WHATSAPP_TOKEN'),
+
+    'phone_number_id' => env('WHATSAPP_PHONE_NUMBER_ID'),
+
+    'graph_version' => env('WHATSAPP_GRAPH_VERSION', 'v25.0'),
+
+    'verify_token' => env('WHATSAPP_VERIFY_TOKEN'),
+
+    'receiver_url' => env('ASAMPEDAS_RECEIVER'),
+
+];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WhatsAppController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/send-message', [WhatsAppController::class,'sendMessage']);
+
+Route::get('/webhook', [WhatsAppController::class,'verify']);
+
+Route::post('/webhook', [WhatsAppController::class,'receive']);
