ec3b0864a4e40d266c67570a470ae129e69d30d9
Raja Faras <rajafaras08@gmail.com>
2026-07-15 13:35:18 +0700

    Initial WhatsApp Gateway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        $middleware->validateCsrfTokens(except: [
+            'webhook',
+            '/webhook',
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
