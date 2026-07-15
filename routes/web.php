d8fc3b49b51f0c6d69e50e862c102d31d0a6e159
Raja Faras <rajafaras08@gmail.com>
2026-07-15 15:27:49 +0700

    debug receive

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WhatsAppController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/test', function () {
+    return 'OK';
 });
 
-// Route::post('/send-message', [WhatsAppController::class,'send']);
+Route::get('/debug', function () {
+    throw new Exception('TEST');
+});
+
 Route::post('/send-message', [WhatsAppController::class,'send']);
 
 Route::get('/webhook', [WhatsAppController::class,'verify']);
